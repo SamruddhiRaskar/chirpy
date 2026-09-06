af87947f56c6414e0d2f0ee215fb62e5b671d96e
Samruddhi Raskar <samruddhiraskar04@gmail.com>
2026-09-06 18:56:20 +0530

    Add metrics and method-specific routes

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
-// This is custom handler we can write this as per our logic. and this custom handler is one type of handle and second is fileserver
-// Here we have created healthzHandler function in which we have pass parameter w is for write a response to the client or browser
-// r is used for accepting the request from the http or client
-// Then we have used various methods to print for get the content-type ,status and plain text msg.
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+// Middleware: increments the counter for every /app request
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// metrics handler
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	fmt.Fprintf(w, "Hits: %d\n", hits)
+}
+
+// /reset handler
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte("Hits reset to 0\n"))
+}
+
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
@@ -15,23 +45,26 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Create API config
+	apiCfg := &apiConfig{}
+
+	// Create router
 	mux := http.NewServeMux()
-	// this creates router which dont have routes yet thats why it is giving 404 not found
 
 	fileserver := http.FileServer(http.Dir("."))
 	// here dir(.) says that look at the current directory means whats in folder
 
 	appHandler := http.StripPrefix("/app", fileserver)
-	//this is strippredix method to remove prefix from dir
 
-	// here "/" means it is asking for root path means root directory is index.html
-	// if it use /style.css or /script.js then it is asking for css file and js file but using html
-	//we can access both css and js due to linking them in html
-	//mux.Handle("/", fileserver)
-	mux.Handle("/app/", appHandler)
+	mux.Handle(
+		"/app/",
+		apiCfg.middlewareMetricsInc(appHandler),
+	)
 
-	mux.HandleFunc("/healthz", healthzHandler)
-	// Now here we have called customHandler func
+	// Other routes
+	mux.HandleFunc("GET /healthz", healthzHandler)
+	mux.HandleFunc("GET /metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("POST /reset", apiCfg.resetHandler)
 
 	server := http.Server{
 		Addr:    ":8080",
